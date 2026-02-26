30f690522fbb4fb4d2b27b5040e3f46ad4c0ca85
oepwqi <oepwqi@163.com>
2026-02-27 00:12:05 +0800

    feat(workflow): add yaml declarative workflow validation and execution

--- src/nini/workflow/__init__.py
+++ src/nini/workflow/__init__.py
@@ -1,1 +1,17 @@
 """工作流模板系统。"""
+
+from nini.workflow.executor import execute_workflow, load_yaml_workflow, load_yaml_workflow_file
+from nini.workflow.template import ValidationResult, WorkflowStep, WorkflowTemplate
+from nini.workflow.validator import detect_cycle, safe_resolve_reference, validate_yaml_workflow
+
+__all__ = [
+    "WorkflowStep",
+    "WorkflowTemplate",
+    "ValidationResult",
+    "execute_workflow",
+    "load_yaml_workflow",
+    "load_yaml_workflow_file",
+    "safe_resolve_reference",
+    "detect_cycle",
+    "validate_yaml_workflow",
+]
